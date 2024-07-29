d819b5730152c0d0e08b4a03ebdb6d77b8a6b2cb
yousef emad <yousefemadkhazbak@gmail.com>
2024-07-29 19:04:48 +0300

    Bulid home screen. + Add icon and image for background home screen. + Add tabs Quran & hadeth & Sebha and Radio. + Theme for app by light mode.

---
+++ lib/home/Tabs/hadeth_tab/hadeth_tab.dart
@@ -0,0 +1,10 @@
+import 'package:flutter/material.dart';
+
+class HadethTab extends StatelessWidget {
+  const HadethTab({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return const Placeholder();
+  }
+}
---
+++ lib/home/Tabs/radio_tabs/radio_tab.dart
@@ -0,0 +1,10 @@
+import 'package:flutter/material.dart';
+
+class RadioTab extends StatelessWidget {
+  const RadioTab({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return const Placeholder();
+  }
+}
