f6056ec116a499685b152c2630de4e32feb790bf
yousef emad <yousefemadkhazbak@gmail.com>
2024-08-29 01:08:54 +0300

    + Shared preference. Fix dark mode for sebha tab.

--- lib/providers/settings_providers/settings_provider.dart
+++ lib/providers/settings_providers/settings_provider.dart
@@ -1,23 +1,56 @@
 import 'package:flutter/material.dart';
+import 'package:shared_preferences/shared_preferences.dart';
 
 class SettingsProvider extends ChangeNotifier {
-  ThemeMode currentTheme = ThemeMode.light;
-  String currentLanguage = 'en';
+  ThemeMode themeMode = ThemeMode.light;
+  Locale locale = const Locale('en');
 
-  void changeLanguage(String newLanguage) {
-    if (currentLanguage == newLanguage) return;
-    currentLanguage = newLanguage;
+  void changeTheme(ThemeMode theme) {
+    themeMode = theme;
+    saveTheme(theme);
     notifyListeners();
   }
 
-  void changeThemeMode(ThemeMode changeTheme) {
-    if (currentTheme == changeTheme) return;
-    currentTheme = changeTheme;
+  void saveTheme(ThemeMode themeMode) async {
+    final SharedPreferences prefs = await SharedPreferences.getInstance();
+    if (themeMode == ThemeMode.light) {
+      await prefs.setString('theme', 'light');
+    } else {
+      await prefs.setString('theme', 'dark');
+    }
+  }
+
+  void changeLocal(String language) {
+    locale = Locale(language);
+    saveLocal(language);
+    notifyListeners();
+  }
+
+  void saveLocal(String language) async {
+    final SharedPreferences prefs = await SharedPreferences.getInstance();
+    prefs.setString('language', language);
+  }
+
+  void getTheme() async {
+    final SharedPreferences prefs = await SharedPreferences.getInstance();
+    String theme = prefs.getString('theme') ?? "light";
+    if (theme == "light") {
+      themeMode = ThemeMode.light;
+    } else {
+      themeMode = ThemeMode.dark;
+    }
+    notifyListeners();
+  }
+
+  void getLanguage() async {
+    final SharedPreferences prefs = await SharedPreferences.getInstance();
+    String language = prefs.getString('language') ?? "en";
+    locale = Locale(language);
     notifyListeners();
   }
 
   String getBackgroundImageName() {
-    return currentTheme == ThemeMode.light
+    return themeMode == ThemeMode.light
         ? 'background_light.png'
         : 'home_dark_background.png';
   }
